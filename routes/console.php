024ba9bc85721830a00594462047f047c28714a8
silvi8794 <silvi8794@gmail.com>
2021-12-22 20:37:05 -0300

    update file console.php

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('logs:clear', function() {
+    exec('rm ' . storage_path('logs/*.log'));
+    $this->comment('Logs have been cleared!');
+})->describe('Clear log files');
