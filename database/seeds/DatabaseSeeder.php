dbeeff2b8df8707877503d350fe6fbcbf1414993
silvi8794 <silvi8794@gmail.com>
2021-12-22 21:54:59 -0300

    update file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('projects')->insert([
+    		'name' => 'Proyecto React', 'description' => 'This project runs with React', 'status'=>'enabled']);
     }
 }
